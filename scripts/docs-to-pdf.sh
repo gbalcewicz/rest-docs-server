93399bd579c46228f83857c42371d613198427b4
Grzegorz Balcewicz <gbalcewicz@kaliop.com>
2018-09-18 13:31:40 +0200

    Removing latex files after pdf generation

--- scripts/docs-to-pdf.sh
+++ scripts/docs-to-pdf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sphinx-build -b latex /docs/$(basename $1) /docs-pdf/$(basename $1)
-cd /docs-pdf/$(basename $1) && make
+cd /docs-pdf/$(basename $1) && make && find . -not \(  -name '*.pdf' -o -name '.' \) -print0 | xargs -0 -I {} rm -rf {}
